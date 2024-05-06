72f351204526c5042281327a17f865cef5d4961f
rorychatt <pavel.rinne@gmail.com>
2024-05-06 20:57:48 +0200

    better map coordinates

--- src/HexGrid.ts
+++ src/HexGrid.ts
@@ -1,26 +1,32 @@
-type Hexagon = {
-    x: number,
-    y: number,
-    z: number // Useful for cubic coordinates in hex grids
+type Vector2D = {
+    x: number;
+    z: number;
 };
 
-class HexGrid {
+class Hexagon {
+    position: Vector2D;
+
+    constructor(position: Vector2D) {
+        this.position = position;
+    }
+}
+
+class HexGridMap {
     grid: Hexagon[][];
 
-    constructor(width: number, height: number) {
+    constructor(mapSize: Vector2D) {
         this.grid = [];
-        for (let x = 0; x < width; x++) {
+        for (let x = 0; x < mapSize.x; x++) {
             this.grid[x] = [];
-            for (let z = 0; z < height; z++) {
-                const y = -x - z;
-                this.grid[x][z] = { x, y, z };
+            for (let z = 0; z < mapSize.z; z++) {
+                this.grid[x][z] = new Hexagon({ x, z });
             }
         }
     }
 
     getGridData() {
         return this.grid;
-      }
+    }
 }
 
-export default HexGrid;
+export default HexGridMap;
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
-import express from 'express';
-import { Server as SocketIOServer } from 'socket.io';
-import http from 'http';
-import HexGrid from './HexGrid';
-import cors from 'cors';
+import express from "express";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import HexGridMap from "./HexGrid";
 
 const app = express();
-app.use(cors({origin: 'http://localhost:8080'}))
+app.use(cors({ origin: "http://localhost:8080" }));
 
 const server = http.createServer(app);
-const io = new SocketIOServer(server,{
-  cors: {
-    origin: 'http://localhost:8080',
-    methods: ["GET", "POST"]
-  }
-}
-);
+const io = new SocketIOServer(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"],
+    },
+});
 
-const hexGrid = new HexGrid(20, 10);
+const mapSettings = {
+    mapSize: {
+        x: 20,
+        z: 10,
+    },
+};
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.emit('mapData', hexGrid.getGridData());
-  
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
+const hexGrid = new HexGridMap(mapSettings.mapSize);
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.emit("mapData", hexGrid.getGridData());
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
-  });
+});
 
 server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log("Server listening on port 3000");
 });
