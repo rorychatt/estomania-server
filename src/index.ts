7ccdccea54c6119c29f20e9c4172154bdfc5884e
rorychatt <pavel.rinne@gmail.com>
2024-05-06 20:07:40 +0200

    cors the stuff

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import express from 'express';
 import { Server as SocketIOServer } from 'socket.io';
 import http from 'http';
 import HexGrid from './HexGrid';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({origin: 'http://localhost:8080'}))
+
 const server = http.createServer(app);
-const io = new SocketIOServer(server);
+const io = new SocketIOServer(server,{
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: ["GET", "POST"]
+  }
+}
+);
 
 const hexGrid = new HexGrid(20, 10);
 
