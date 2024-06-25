e9cdc71a1fbc355356ac8535e27520b5a8453b05
rorychatt <pavel.rinne@gmail.com>
2024-06-25 15:39:19 +0200

    better styling

--- src/index.ts
+++ src/index.ts
@@ -10,31 +10,31 @@ app.use(cors({ origin: "http://localhost:8080" }));
 
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
-    cors: {
-        origin: "http://localhost:8080",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+  },
 });
 
 const gameSettings = {
-    mapSize: {
-        x: 20,
-        z: 10,
-    },
+  mapSize: {
+    x: 20,
+    z: 10,
+  },
 };
 
-const game = new Game(gameSettings)
+const game = new Game(gameSettings);
 
 io.on("connection", (socket) => {
-    console.log("A user connected");
-    game.addPlayer(new Player(socket.id))
-    socket.emit("gameData", game);
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-        game.removePlayerBySocketID(socket.id)
-    });
+  console.log("A user connected");
+  game.addPlayer(new Player(socket.id));
+  socket.emit("gameData", game);
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    game.removePlayerBySocketID(socket.id);
+  });
 });
 
 server.listen(3000, () => {
-    console.log("Server listening on port 3000");
+  console.log("Server listening on port 3000");
 });
